aeb4cee5d4f1693cc913161b778faa9fc735d925
JuanDDegli <juanddegliesposte@gmail.com>
2023-01-30 09:43:16 -0300

    Subindo Pasta do Curso

---
+++ Aula011_ D/Ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.000001
+console.log (`A velocidade do seu carro é de ${vel}Km/h`)
+if (vel > 60){
+    console.log('Você foi Multado!')
+}
+console.log('Dirija sempre com segurança!')
---
+++ Aula012_D/Ex014/script.js
@@ -0,0 +1,20 @@
+function carregar() {
+var msg = document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora = 20 //data.getHours()
+var minutos = data.getMinutes()
+
+msg.innerHTML = `Agora são exatamente ${hora} horas e ${minutos} minutos.`
+if (hora >= 0 && hora < 12){
+    img.src = "manha.png"
+    document.body.style.background = '#a8dafb   '
+}else if (hora >= 12 && hora < 18){
+    img.src = 'tarde.png'
+    document.body.style.background = '#ff8b41'
+}else{
+    img.src = 'fotonoite.png'
+    document.body.style.background = '#001642'
+}
+} //#001642
+//#a8dafb
