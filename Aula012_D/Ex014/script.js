50dc969e63c061dfa1e07af8cc17115e177df69e
JuanDDegli <juanddegliesposte@gmail.com>
2023-01-30 09:49:09 -0300

    .

--- Aula012_D/Ex014/script.js
+++ Aula012_D/Ex014/script.js
@@ -2,7 +2,7 @@ function carregar() {
 var msg = document.getElementById('msg')
 var img = window.document.getElementById('imagem')
 var data = new Date()
-var hora = 20 //data.getHours()
+var hora = data.getHours()
 var minutos = data.getMinutes()
 
 msg.innerHTML = `Agora são exatamente ${hora} horas e ${minutos} minutos.`
